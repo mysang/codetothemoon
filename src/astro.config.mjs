c9ac8a70c45b50a03c2478bf62b9d7025433a1eb
Nguyen Sang <sangnck@gmail.com>
2024-04-30 16:43:34 +0700

    Update

--- src/astro.config.mjs
+++ src/astro.config.mjs
@@ -9,6 +9,5 @@ export default defineConfig({
   integrations: [mdx(), sitemap(), tailwind(), react()],
   site: import.meta.env.MODE === 'development' ? 'http://localhost:4321' : 'https://codetothemoon.com',
   output: 'static',
-  base: 'docs',
   outDir: '../docs',
 });
